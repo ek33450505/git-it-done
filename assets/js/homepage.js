aaf247dd159071eaec9dcc623fcebc1558836dd6
Ed Kubiak <ek33450505@gmail.com>
2022-03-07 18:51:02 -0500

    added event buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -90,5 +91,35 @@ for (var i = 0; i < repos.length; i++) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+
+buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+    console.log(language);
+
+    if (language) {
+      getFeaturedRepos(language);
+
+      // clear old content
+      repoContainerEl.textContent = "";
+    }
+};
+
+
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+
+// add event listener to the div elements
+languageButtonsEl.addEventListener("click", buttonClickHandler);
